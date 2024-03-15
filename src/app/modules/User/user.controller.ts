527ecf4832fb751d5ee5c67124dcd58edce6932c
Upoma21801~ <upoma.cdsr@gmail.com>
2024-03-16 04:23:00 +0600

    admin created successfully with hashed password

--- src/app/modules/User/user.controller.ts
+++ src/app/modules/User/user.controller.ts
@@ -2,7 +2,8 @@ import { Request, Response } from "express";
 import { userServices } from "./user.service";
 
 const createAdmin = async (req: Request, res: Response) => {
-  const result = await userServices.createAdminIntoDB();
+  //   console.log();
+  const result = await userServices.createAdminIntoDB(req.body);
   res.send(result);
 };
 
