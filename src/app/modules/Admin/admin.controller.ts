e12f6f471c6b104ac74e9a02ee43e9759334239a
Upoma21801~ <upoma.cdsr@gmail.com>
2024-03-21 03:42:56 +0600

    new response pattern added

--- src/app/modules/Admin/admin.controller.ts
+++ src/app/modules/Admin/admin.controller.ts
@@ -1,4 +1,4 @@
-import { Request, Response } from "express";
+import { Request, RequestHandler, Response } from "express";
 import httpStatus from "http-status";
 import catchAsync from "../../../shared/catchAsync";
 import pick from "../../../shared/pick";
@@ -6,27 +6,23 @@ import sendResponse from "../../../shared/sendResponse";
 import { adminFilterableFields } from "./admin.constant";
 import { AdminService } from "./admin.service";
 
-const getAllFromDB = async (req: Request, res: Response) => {
-  try {
+const getAllFromDB: RequestHandler = catchAsync(
+  async (req: Request, res: Response) => {
     // console.log(req.query)
     const filters = pick(req.query, adminFilterableFields);
     const options = pick(req.query, ["limit", "page", "sortBy", "sortOrder"]);
     console.log(options);
     const result = await AdminService.getAllFromDB(filters, options);
-    res.status(200).json({
+
+    sendResponse(res, {
+      statusCode: httpStatus.OK,
       success: true,
-      message: "Admin data fetched!",
+      message: "Admin data fetched!!",
       meta: result.meta,
       data: result.data,
     });
-  } catch (err: any) {
-    res.status(500).json({
-      success: false,
-      message: err?.name || "Something went wrong",
-      error: err,
-    });
   }
-};
+);
 
 const getById = catchAsync(async (req: Request, res: Response) => {
   const { id } = req.params;
