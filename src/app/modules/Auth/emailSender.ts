6d387027eb3b9e04c03dba9e9a85662fdb46ddc6
Upoma21801~ <upoma.cdsr@gmail.com>
2024-03-25 04:04:36 +0600

    file uploaded with multer and cloudinary

--- src/app/modules/Auth/emailSender.ts
+++ src/app/modules/Auth/emailSender.ts
@@ -16,7 +16,7 @@ const emailSender = async (email: string, html: string) => {
   });
 
   const info = await transporter.sendMail({
-    from: '"PH Health Care" <fahimfiroz.ph@gmail.com>', // sender address
+    from: '"PH Health Care" <upoma.cdsr@gmail.com>', // sender address
     to: email, // list of receivers
     subject: "Reset Password Link", // Subject line
     //text: "Hello world?", // plain text body
--- src/app/modules/User/user.routes.ts
+++ src/app/modules/User/user.routes.ts
@@ -1,10 +1,16 @@
 import { UserRole } from "@prisma/client";
 import express from "express";
+import { fileUploader } from "../../../helpars/fileUploader";
 import auth from "../../middlewares/auth";
 import { userController } from "./user.controller";
 
 const router = express.Router();
 
-router.post("/", auth(UserRole.superAdmin), userController.createAdmin);
+router.post(
+  "/",
+  auth(UserRole.superAdmin),
+  fileUploader.upload.single("file"),
+  userController.createAdmin
+);
 
 export const userRoutes = router;
